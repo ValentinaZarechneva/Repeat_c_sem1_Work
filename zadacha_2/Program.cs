d3b504c5637c2023c10d200093c09e592e66f2a3
Valentina Zarechneva <valvas_07@mail.ru>
2023-09-27 11:46:23 +0300

    задача2, выведение большего или меньшего значения на экран

---
+++ zadacha_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Напишите программу, 
+// которая на вход принимает два числа и 
+// выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write(a);
+    Console.WriteLine("большее число");
+    Console.Write(b);
+    Console.WriteLine("меньшее число");
+}
+else if(b > a) 
+{
+    Console.WriteLine(b);
+    Console.WriteLine("большее число");
+    Console.WriteLine(a);
+    Console.WriteLine("меньшее число");
+}
+else if(a == b)
+{
+    Console.WriteLine("Значение чисел равно");
+}
